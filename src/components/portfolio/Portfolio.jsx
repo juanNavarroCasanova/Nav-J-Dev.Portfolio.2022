cfeca84dc95636773103f3d77cf5f31eb3010a36
Juan Navarro Casanova <navakasa1979@gmail.com>
2021-11-08 18:40:24 +1100

    changes to portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,7 +9,7 @@ export default function Portfolio(props) {
         <div className="portfolio" id="portfolio">
             <div className="container">
                 <div className="item">
-                    <img src="/assets/web.png" alt=""/>
+                    <img src="/assets/web.png" alt="Image of website background"/>
                     <div className="description">
                         <div className="icon">
                             <a href="https://github.com/juanNavarroCasanova/portfolio" target="_blank" rel="noopener noreferrer">Website Repository</a> 
@@ -17,14 +17,14 @@ export default function Portfolio(props) {
                     </div>
                 </div>
                 <div className="item construction">
-                    <img src="/assets/covid-map.jpg" alt=""/>
+                    <img src="/assets/covid-map.jpg" alt="Image of Covid app"/>
                     <div className="description">
                         <h3>COVID Tracker</h3>
                         <h2>Under Construction</h2>
                     </div>
                 </div>
                 <div className="item construction">
-                    <img src="/assets/hand-laptop.jpg" alt=""/>
+                    <img src="/assets/hand-laptop.jpg" alt="Image of Roster App"/>
                     <div className="description">
                         <h3>Roster App</h3>
                         <h2>Under Construction</h2>
